fee607f7a5b2738b186388893eba33617c658b09
seungmin-dev <seungmin-dev@gmail.com>
2020-07-30 23:33:24 +0900

    script.js 내용 추가

--- js/script.js
+++ js/script.js
@@ -17,4 +17,16 @@ $(document).ready(function () {
     for (i = 0; i < 3; i++) {
         $('.package').eq(i).find('a').css('background-image', 'url(./src/package' + i + '.jpg');
     }
+    
+    // profile-image
+    for (i = 0; i < 4; i++) {
+        $('.profile-image').eq(i).css('background-image', 'url(./src/profile' + i + '.png');
+    }
+    
+    // review-image
+    for (i = 0; i < 4; i++) {
+        $('.review-image').eq(i).css('background-image', 'url(./src/review' + i + '.jpg');
+    }
+    
+   
 });
